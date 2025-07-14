17f980f97b610e1da21aa50eb38f9a302d12ab62
Mahmud-Alam <official.mahmud23@gmail.com>
2025-07-15 00:08:19 +0600

    created home page content and home page content img repo

---
+++ src/main/java/com/mahmudalam/bmu_ws/repository/HomePageContentImageRepository.java
@@ -0,0 +1,7 @@
+package com.mahmudalam.bmu_ws.repository;
+
+import com.mahmudalam.bmu_ws.model.HomePageContentImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HomePageContentImageRepository extends JpaRepository<HomePageContentImage, Long> {
+}
---
+++ src/main/java/com/mahmudalam/bmu_ws/repository/HomePageContentRepository.java
@@ -0,0 +1,7 @@
+package com.mahmudalam.bmu_ws.repository;
+
+import com.mahmudalam.bmu_ws.model.HomePageContent;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HomePageContentRepository extends JpaRepository<HomePageContent, Long> {
+}
